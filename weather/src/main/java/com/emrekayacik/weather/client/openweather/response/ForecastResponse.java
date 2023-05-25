f8378b5cfc423c15f4513491ac99a4214550d412
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2023-05-25 21:12:51 +0300

    Main forecast fields added.

--- weather/src/main/java/com/emrekayacik/weather/client/openweather/response/ForecastResponse.java
+++ weather/src/main/java/com/emrekayacik/weather/client/openweather/response/ForecastResponse.java
@@ -6,9 +6,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 public class ForecastResponse {
     private ForecastCity city;
+    private ArrayList<WeatherResponse> list;
 }
